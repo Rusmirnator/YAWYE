e92c534781b3643be59543ef16c2ea34f21e7a3f
Rusmirnator <ruson80@gmail.com>
2021-03-27 02:13:16 +0100

    API implementation , app harbour deployment

--- YAWYE/Startup.cs
+++ YAWYE/Startup.cs
@@ -29,6 +29,7 @@ namespace YAWYE
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +53,12 @@ namespace YAWYE
 
             app.UseAuthentication();
             app.UseAuthorization();
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
