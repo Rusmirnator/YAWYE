1a02837c9724323aa71888bee7d038fc84ee72eb
Marcin Rusinek <ruson80@gmail.com>
2021-03-27 01:31:26 +0100

    API implementation

---
+++ YAWYE/API/ProductsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YAWYE.Core;
+using YAWYE.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace YAWYE.API
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly YAWYEDbContext context;
+
+        public ProductsController(YAWYEDbContext context)
+        {
+            this.context = context;
+        }
+        // GET: api/<ProductsController>
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return context.Products;
+        }
+
+        // GET api/<ProductsController>/5
+        [HttpGet("{id}")]
+        public Product Get(int id)
+        {
+            return context.Products.SingleOrDefault(p => p.ProductId == id);
+        }
+
+        // POST api/<ProductsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ProductsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
